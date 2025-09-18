81c595d4e1df838863d2d9888fcab0a704246626
Elliot Nicolás <A00840918@tec.mx>
2025-09-18 09:48:52 -0600

    Update canon.py

--- canon.py
+++ canon.py
@@ -55,9 +55,9 @@ def move():
 
     draw()
 
-    for target in targets:
-        if not inside(target):
-            return
+    #for target in targets:
+    #    if not inside(target):
+    #        return
 
     ontimer(move, 50)
 
